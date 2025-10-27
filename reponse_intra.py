601ea7b78ee3deb400ccbdfb159b234621792397
6242693 <6242693@etu.cegepsth.qc.ca>
2025-10-27 12:30:42 -0400

    Début Q2

--- reponse_intra.py
+++ reponse_intra.py
@@ -23,14 +23,21 @@ question1()
 # Question 2
 import numpy as np
 import matplotlib.pyplot as plt
+def question2():
+#    heure = 0
+    population_initiale=float(input("Entrez la population initiale de bactéries:"))
+#    for heure in range(0,11):
+    population= population_initiale + np.pi/1.5
+    return population
 
 # a) demande population initiale de bacteries (input)
 # b) pendant 10h calcule population a chaque heure(compteur=0)
 # augmentation population= np.pi / 1.5 a chaque heure
+question2()
 # c)
 plt.xlabel("Heure")
 plt.ylabel("Population")
-# plt.title
+plt.title("Croissance bactérienne")
 # plt.plot([x],[y],"*b")
 plt.plot([0,10],[50000,50000],"r--")
 plt.grid()
