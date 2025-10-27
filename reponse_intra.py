8cf49dbd50432b1e7ddf682d964272f36df7d305
6242693 <6242693@etu.cegepsth.qc.ca>
2025-10-27 12:03:13 -0400

    Q1

--- reponse_intra.py
+++ reponse_intra.py
@@ -1,6 +1,7 @@
 ##########################################
 # Brodeur, Maélie, <6242693>
 ##########################################
+import random
 # temperature_ideale entre 24 et 30°C
 # a) aleatoirement 10 temperatures entre 20 et 35°C arrodies a 1 decimale (round)
 # b) affiche chaque temperature avec so jour (compteur)
@@ -9,3 +10,19 @@
 # print("Trop froid") si inferieure a borne inf de temperature ideale (24)
 # print("Trop chaud" si superieure a borne sup de temperature ideale (30)
 # quand 10 temperatures affichées print("Fin")
+
+def question1():
+    for jour in range(1,11):
+#   random.seed(42)
+        temperature=random.uniform(20,35)
+        print("Jour",jour,":",round(temperature,1),"°C")
+        if 24<=temperature<=30:
+            print("OK")
+        elif temperature < 24:
+            print("Trop froid")
+        elif temperature > 30:
+            print("Trop chaud")
+
+    print("Fin")
+
+question1()
