a9c1e513d4ad0a73a1cf84f0945e17669eabfd70
6242693 <6242693@etu.cegepsth.qc.ca>
2025-10-27 11:37:42 -0400

    Consignes Q1

--- reponse_intra.py
+++ reponse_intra.py
@@ -1,3 +1,11 @@
 ##########################################
-# Nom, Prénom, <Numéro de DA>
+# Brodeur, Maélie, <6242693>
 ##########################################
+# temperature_ideale entre 24 et 30°C
+# a) aleatoirement 10 temperatures entre 20 et 35°C arrodies a 1 decimale (round)
+# b) affiche chaque temperature avec so jour (compteur)
+# c)
+# print("OK") si dans temperature ideale
+# print("Trop froid") si inferieure a borne inf de temperature ideale (24)
+# print("Trop chaud" si superieure a borne sup de temperature ideale (30)
+# quand 10 temperatures affichées print("Fin")
